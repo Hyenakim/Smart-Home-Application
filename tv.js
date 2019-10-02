4fb7846cbc6c27d319ee191119c6468a43dca856
Hyenakim <gpsk1202@naver.com>
2019-10-02 20:38:46 +0900

    add tv module

--- tv.js
+++ tv.js
@@ -1,25 +1,36 @@
-var power = false;
-var channel = 1;
+var power
+var channel
 function On(){
-    if(!power)
+    console.log(power)
+    if(power)
+        console.log("이미 켜져있습니다.");
+    else{
         power = true;
-    else
-        Console.log("이미 켜져있습니다.");
+        console.log("tv가 켜졌습니다.");
+    }
+    return power   
 }
 function Off(){
-    if(power)
+    if(!power)
+        console.log("이미 꺼져있습니다.");
+    else{
         power = false;
-    else
-        Console.log("이미 꺼져있습니다.");
+        console.log("tv가 꺼졌습니다.");
+    }
+    return poser
 }
 function SetChannel(n){
+    console.log(channel);
     if(channel == n){
-        Console.log("현재 "+n+"번 채널을 시청하고 계십니다.");
+        console.log("현재 "+n+"번 채널을 시청하고 계십니다.");
         channel = n;
     }
     else
-        Console.log("채널이 "+n+"번으로 변경되었습니다");
+        console.log("채널이 "+n+"번으로 변경되었습니다.");
+    return channel
 }
+module.exports.power = power;
+module.exports.channel = channel;
 module.exports.on = On;
 module.exports.off = Off;
 module.exports.setChannel = SetChannel;
