b1c1eec3dd9229b7d93e314f1728b539d14c6eed
sy2298 <39763694+sy2298@users.noreply.github.com>
2019-10-06 13:49:19 +0900

    Create Lamp.js

---
+++ Lamp.js
@@ -0,0 +1,33 @@
+const SmartAppliance =require('./SmartAppliance');
+var rl = require('readline');
+var prompts = rl.createInterface({input: process.stdin,output:process.stdout,terminal:false});
+class Lamp extends SmartAppliance{
+    constructor(brightness){
+    super();
+    this.brightness = brightness;
+  }
+     turn_on(){
+        console.log('---     밝기 선택     ---');
+        console.log('---  1단계 수면, 휴식  ---');
+        console.log('---  2단계 독서,일상   ---');
+        console.log('---  3단계 공부,사무   ---');  
+        prompts.question('밝기를 선택해주세요: ',(light)=>{
+            if(light==1){
+                   this.brightness=1;
+                   console.log('밝기 1단계입니다.');
+                   prompts.pause();
+                }
+                else if(light==2){
+                this.brightness=2; 
+                 console.log('밝기 2단계입니다.');
+                prompts.pause();
+                }
+                else if(light==3){
+                    this.brightness=3; 
+                     console.log('밝기 3단계입니다.');
+                     prompts.pause();
+                 }
+             })
+    }
+}
+module.exports = Lamp
