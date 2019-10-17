ada7106e6a51c38834bf3c10403eda5733ec9e09
sy2298 <39763694+sy2298@users.noreply.github.com>
2019-10-17 19:45:59 +0900

    Update Tv.js

--- Tv.js
+++ Tv.js
@@ -3,106 +3,57 @@ var rl_sync = require('readline-sync')
 
 
 class Tv extends SmartAppliance{
-     
-      constructor(channel,vol){
-      super("Tv")
-
-      this.channel = channel;
-      this.vol = vol;
-      console.log('On? Off')
-      const str = rl_sync.prompt()
-      if(str == "On"){
-         super.getCommand(str,"Tv");
-         console.log('1. 볼륨 높이기');
-         console.log('2. 볼륨 낮추기');
-         console.log('3. 채널 높이기');
-         console.log('4. 채널 낮추기');
-         console.log('5. 설정 완료');  
-         while(true){
-            const num = rl_sync.prompt()
-               if(num=='1'){
-               vol++;
-               console.log("볼륨은 "+vol+" 입니다.");
-               }
-               else if(num==2){
-               vol--;
-               console.log("볼륨은 "+vol+" 입니다.");
-               }
-               else if(num==3){
-               channel++;
-               console.log("채널은 "+channel+"입니다.");
-               }
-               else if(num==4){
-               channel--;
-               console.log("채널은 "+channel+"입니다.");
-               }
-               else if(num==5){
-                  break;
-               }
-               else{
-            console.log('??3')
-
-                  console.log('1과 5사이 숫자만 입력해주세요.')
-                  continue
-               }       
-         }
-     
-   //    prompts.question(" On? Off  :",(str)=>{
-   //     if(str == "On"){
-   //          super.getCommand(str,"Tv");
-   //          prompts.pause();
-   //       console.log('1. 볼륨 높이기');
-   //      console.log('2. 볼륨 낮추기');
-   //      console.log('3. 채널 높이기');
-   //      console.log('4. 채널 낮추기');
-   //      console.log('5. 설정 완료');  
-   //       prompts.setPrompt('>');
-   //       prompts.prompt()
-   //       prompts.on('line',function(num){
-       
-   //        if(num==1){
-   //         vol++;
-   //          console.log("볼륨은 "+vol+" 입니다.");
-   //          prompts.setPrompt('>');
-   //          prompts.prompt()
-   //           }
-   //        else if(num==2){
-   //         vol--;
-   //         console.log("볼륨은 "+vol+" 입니다.");
-   //         prompts.setPrompt('>');
-   //       prompts.prompt()
-   //        }
-   //        else if(num==3){
-   //          channel++;
-   //          console.log("채널은 "+channel+"입니다.");
-   //          prompts.setPrompt('>');
-   //       prompts.prompt()
-   //        }
-   //        else if(num==4){
-   //          channel--;
-   //          console.log("채널은 "+channel+"입니다.");
-   //          prompts.setPrompt('>');
-   //       prompts.prompt()
-   //        }
-   //        else if(num==5){
-      
-   //           prompts.pause()
-   //          }
-   //         else{
-   //            console.log('1과 5사이 숫자만 입력해주세요.')
-   //            prompts.setPrompt('>');
-   //            prompts.prompt()
-   //         }  
-   //         });
-   //      }
-   //      else if(str=="Off"){
-   //       super.getCommand(str,"Tv");
-         
-   //      }
-      
-   //   })
-      }
-      }
+   
+   constructor(channel,vol){
+   super("Tv")
+   this.channel = channel;
+   this.vol = vol;
    }
-
+   async menu(){
+      await new Promise((resolve,reject)=>{
+      var num = 0
+      while(num!=5){
+      console.log('1. 볼륨 높이기');
+      console.log('2. 볼륨 낮추기');
+      console.log('3. 채널 높이기');
+      console.log('4. 채널 낮추기');
+      console.log('5. 설정 완료');  
+      while(true){
+         num = rl_sync.prompt()
+            if(num=='1'){
+           this.vol++;
+            console.log("볼륨은 "+this.vol+" 입니다.");
+            }
+            else if(num==2){
+               this.vol--;
+            console.log("볼륨은 "+this.vol+" 입니다.");
+            }
+            else if(num==3){
+               this.channel++;
+            console.log("채널은 "+this.channel+"입니다.");
+            }
+            else if(num==4){
+               this.channel--;
+            console.log("채널은 "+this.channel+"입니다.");
+            }
+            else if(num==5){
+               resolve();
+               break;
+            }
+            else{
+               console.log('1과 5사이 숫자만 입력해주세요.')
+               continue
+            }
+         }       
+      }
+   })
+}
+   
+     
+ 
+ 
+      
+        }
+      
+   
 module.exports = Tv
